7dc1f6708c34fd7c37e2fa94c077a2076ea4ea0f
Samuel Darkhovsky <darkhovsky@usa.net>
2016-10-06 19:23:42 -0700

    Calculating the mass center and orientation using PCA

--- OWIFeatCalcPrediction/OWIFeatCalcPrediction.cpp
+++ OWIFeatCalcPrediction/OWIFeatCalcPrediction.cpp
@@ -167,6 +167,76 @@ void calc_convexHull(cv::Mat src, cv::Mat& dst)
 */
 }
 
+
+void drawAxis(cv::Mat& img, cv::Point p, cv::Point q, cv::Scalar colour, const float scale = 0.2)
+{
+    double angle;
+    double hypotenuse;
+    angle = atan2( (double) p.y - q.y, (double) p.x - q.x ); // angle in radians
+    hypotenuse = sqrt( (double) (p.y - q.y) * (p.y - q.y) + (p.x - q.x) * (p.x - q.x));
+//    double degrees = angle * 180 / CV_PI; // convert radians to degrees (0-180 range)
+//    cout << "Degrees: " << abs(degrees - 180) << endl; // angle in 0-360 degrees range
+    // Here we lengthen the arrow by a factor of scale
+    q.x = (int) (p.x - scale * hypotenuse * cos(angle));
+    q.y = (int) (p.y - scale * hypotenuse * sin(angle));
+    cv::line(img, p, q, colour, 1, CV_AA);
+    // create the arrow hooks
+    p.x = (int) (q.x + 9 * cos(angle + CV_PI / 4));
+    p.y = (int) (q.y + 9 * sin(angle + CV_PI / 4));
+    cv::line(img, p, q, colour, 1, CV_AA);
+    p.x = (int) (q.x + 9 * cos(angle - CV_PI / 4));
+    p.y = (int) (q.y + 9 * sin(angle - CV_PI / 4));
+    cv::line(img, p, q, colour, 1, CV_AA);
+}
+
+void calc_event_mc_and_orientation(cv::Mat feat_event)
+{
+	int feat_event_area = 0;
+	for(int r = 0; r < feat_event.rows; ++r){
+		for(int c = 0; c < feat_event.cols; ++c){
+			if (feat_event.at<int>(r, c) != 0)
+				feat_event_area++;
+		 }
+	}
+
+    cv::Mat data_pts = cv::Mat(feat_event_area, 2, CV_64FC1);
+	int i = 0;
+	for(int r = 0; r < feat_event.rows; ++r){
+		for(int c = 0; c < feat_event.cols; ++c){
+			if (feat_event.at<int>(r, c) != 0)
+			{
+				data_pts.at<double>(i, 0) = c;
+				data_pts.at<double>(i, 1) = r;
+				i++;
+			}
+		 }
+	}
+
+    //Perform PCA analysis
+    cv::PCA pca_analysis(data_pts, cv::Mat(), CV_PCA_DATA_AS_ROW);
+    //Store the center of the object
+    cv::Point cntr = cv::Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)),
+                      static_cast<int>(pca_analysis.mean.at<double>(0, 1)));
+    //Store the eigenvalues and eigenvectors
+    std::vector<cv::Point2d> eigen_vecs(2);
+    std::vector<double> eigen_val(2);
+    for (int i = 0; i < 2; ++i)
+    {
+        eigen_vecs[i] = cv::Point2d(pca_analysis.eigenvectors.at<double>(i, 0),
+                                pca_analysis.eigenvectors.at<double>(i, 1));
+        eigen_val[i] = pca_analysis.eigenvalues.at<double>(0, i);
+    }
+    // Draw the principal components
+    cv::circle(feat_event, cntr, 3, cv::Scalar(80), 2);
+    cv::Point p1 = cntr + 0.02 * cv::Point(static_cast<int>(eigen_vecs[0].x * eigen_val[0]), static_cast<int>(eigen_vecs[0].y * eigen_val[0]));
+    cv::Point p2 = cntr - 0.02 * cv::Point(static_cast<int>(eigen_vecs[1].x * eigen_val[1]), static_cast<int>(eigen_vecs[1].y * eigen_val[1]));
+    drawAxis(feat_event, cntr, p1, cv::Scalar(80), 1);
+    drawAxis(feat_event, cntr, p2, cv::Scalar(80), 5);
+    double angle = atan2(eigen_vecs[0].y, eigen_vecs[0].x); // orientation in radians
+	cv::imshow("feat_event", feat_event);
+	cv::waitKey(0);
+}
+
 /*
 read a training image
 apply a feature selecting yellow regions
@@ -276,10 +346,12 @@ int main(int argc, char** argv)
 
 				cv::Mat component_hull;
 				calc_convexHull(component_img, component_hull);
-				cv::Mat event;
-				cv::bitwise_and(feat_img, component_hull, event);
-				cv::imshow("event", event);
+				cv::Mat feat_event;
+				cv::bitwise_and(feat_img, component_hull, feat_event);
+				cv::imshow("feat_event", feat_event);
 				cv::waitKey(0);
+
+				calc_event_mc_and_orientation(feat_event);
 			}
 		}
 
