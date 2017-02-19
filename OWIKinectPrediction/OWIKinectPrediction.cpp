61a5a174d984185ae1a6980faa2654ea22c1c250
Samuel Darkhovsky <sdarkhovsky@euclidiq.com>
2017-02-19 12:51:26 -0800

    detecting corners from u,v coordinates

--- OWIKinectPrediction/OWIKinectPrediction.cpp
+++ OWIKinectPrediction/OWIKinectPrediction.cpp
@@ -236,7 +236,7 @@ int main(int argc, char** argv)
 #else
         generate_point_cloud_prism(point_cloud);
 #endif
-#if 1
+#if 0
         Vector3f max_xyz = Vector3f(0.04, 0.2, 0.8f);
         Vector3f min_xyz = Vector3f(-0.04, -0.2, 0);
         point_cloud.filter_by_xyz(min_xyz, max_xyz);
